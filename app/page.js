7fe38cfa2cbd50bbe1d1c1e4296310c23d2a9440
Trainerbot <hasainulh@gmail.com>
2026-08-11 18:26:38 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>AIVI Orders Efficiency Dashboard</h1>
+      <p>Dashboard is successfully deployed on Vercel!</p>
+    </div>
+  );
+}
